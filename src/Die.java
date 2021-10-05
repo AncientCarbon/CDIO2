65127859223d3c0d87e88dbd9a3be84c9a68520d
Silja Sandersen <silja.sandersen@hotmail.com>
2021-10-05 16:45:06 +0200

    Definerer faceValue i Die klassen

--- src/Die.java
+++ src/Die.java
@@ -4,6 +4,7 @@
 
 public class Die {
     int sides; //starter med værdien 0
+    int faceValue;
 
     public Die(int _sides){
         this.sides = _sides; // sides angives som det givne _sides, som bliver givet i Dicegame-klassen
@@ -11,7 +12,8 @@ public class Die {
 
     public int roll(){ //funktionen roll ruller en terning med x-antal sider.
         // Metode der kan køre når man har lavet et Dice objekt
-        return (int) Math.floor(Math.random()*this.sides+1); //Maksimum værdi på terningen = 6. Minimum værdi på terningen = 1
+        faceValue = (int) Math.floor(Math.random()*this.sides+1);
+        return faceValue; //Maksimum værdi på terningen = 6. Minimum værdi på terningen = 1
         // Maksimum værdi på terningen = 6.
         // Minimum værdi på terningen = 1
     }
--- src/FeltSpil.java
+++ src/FeltSpil.java
@@ -1,4 +1,4 @@
-// Version 0.0.1
+// Version 0.0.2
 
 import java.util.*;
 public class FeltSpil {
