4e946dfca666e646f24c793f3504ec34530e1d8d
Silja Sandersen <silja.sandersen@hotmail.com>
2021-10-05 16:46:32 +0200

    Lavet et terninge-objekt mere. terning2

--- src/FeltSpil.java
+++ src/FeltSpil.java
@@ -5,7 +5,8 @@ public class FeltSpil {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        Die terning = new Die(6);
+        Die terning1 = new Die(6);
+        Die terning2 = new Die(6);
         Player player1 = new Player(in.nextLine());
         Player player2 = new Player(in.nextLine());
 
