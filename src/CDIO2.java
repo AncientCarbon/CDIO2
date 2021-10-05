1fc8b675840f86335c3f566c51a48b38b6e578b3
Silja Sandersen <silja.sandersen@hotmail.com>
2021-10-05 14:33:57 +0200

    Die klasse og Player klasse indsat fra CDIO 1

--- src/CDIO2.java
+++ src/CDIO2.java
@@ -1,3 +1,5 @@
+// Version 0.0.1
+
 import java.util.*;
 public class CDIO2 {
     public static void main(String[] args) {
---
+++ src/Player.java
@@ -0,0 +1,9 @@
+public class Player {
+    public int score;
+    public String name;
+
+    public Player(String _name){
+        this.score = 0;
+        this.name = _name;
+    }
+}
