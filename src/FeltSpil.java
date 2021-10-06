e15a0cabff014adeff5750bef348f4ec79ec2186
Anton Ludvigsen <boblo.ludvigsen@gmail.com>
2021-10-06 14:45:04 +0200

    player1 og player2 har fået roller og navne

--- src/FeltSpil.java
+++ src/FeltSpil.java
@@ -5,13 +5,18 @@ public class FeltSpil {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        Die terning1 = new Die(1,1);
-        Die terning2 = new Die(6, 6);
+        Die terning1 = new Die(1,6);
+        Die terning2 = new Die(1, 6);
         System.out.println(terning1.roll());
         System.out.println(terning2.roll());
 
+        System.out.print("Player 1 skriv dit navn: ");
         Player player1 = new Player(in.nextLine());
+        System.out.print("Player 2 skriv dit navn: ");
         Player player2 = new Player(in.nextLine());
+
+        System.out.println("De to spillere hedder " + player1.name + " og " + player2.name);
+
 
         Felt F2 = new Felt ("Tower", 250); // Tower
         Felt F3 = new Felt ("Crater", -100); // Crater
@@ -24,8 +29,6 @@ public class FeltSpil {
         Felt F10 = new Felt ("The Werewall", -80); // The Werewall (Werewolf-Wall)
         Felt F11 = new Felt ("The Pit", -50); // The Pit
         Felt F12 = new Felt ("The Goldmine", 650); // The Goldmine
-
-
-
+        
     }
 }
