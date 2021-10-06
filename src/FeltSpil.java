6d59af93e99b021745e5961bb4f48b1d6582298f
Anton Ludvigsen <boblo.ludvigsen@gmail.com>
2021-10-06 15:55:15 +0200

    Begge players har en tur nu

--- src/FeltSpil.java
+++ src/FeltSpil.java
@@ -33,8 +33,9 @@ public class FeltSpil {
 
         while (player1.balance < 3000 && player2.balance < 3000){
 
-            System.out.print("Tryk 'enter' for at rulle med terningerne:");
+            System.out.print("\n"+ player1.name + "'s tur. Tryk 'enter' for at rulle med terningerne:");
             new Scanner(System.in).nextLine();
+            System.out.println();
 
             faceTotal = terning1.roll() + terning2.roll();
 
@@ -86,6 +87,78 @@ public class FeltSpil {
                 System.out.println("Der findes kun felter 2-12. Du har slået " + faceTotal);
             }
             System.out.println(player1.name + "'s nye score er: " + player1.balance);
+
+
+
+            //player 2's tur
+            System.out.println("\n" + player2.name + "'s tur. Tryk enter for at rulle med terningerne");
+            new Scanner(System.in).nextLine();
+            System.out.println();
+
+            faceTotal = terning1.roll() + terning2.roll();
+
+            if (faceTotal == F2.felt){
+                player2.balance += F2.point;
+                System.out.println("Du er landet på " + F2.name + ", og du har fået " + F2.point + " point.");
+            }
+            else if (faceTotal == F3.felt){
+                player2.balance += F3.point;
+                System.out.println("Du er landet på " + F3.name + ", og du har fået " + F3.point + " point.");
+            }
+            else if (faceTotal == F4.felt){
+                player2.balance += F4.point;
+                System.out.println("Du er landet på " + F4.name + ", og du har fået " + F4.point + " point.");
+            }
+            else if (faceTotal == F5.felt){
+                player2.balance += F5.point;
+                System.out.println("Du er landet på " + F5.name + ", og du har fået " + F5.point + " point.");
+            }
+            else if (faceTotal == F6.felt){
+                player2.balance += F6.point;
+                System.out.println("Du er landet på " + F6.name + ", og du har fået " + F6.point + " point.");
+            }
+            else if (faceTotal == F7.felt){
+                player2.balance += F7.point;
+                System.out.println("Du er landet på " + F7.name + ", og du har fået " + F7.point + " point.");
+            }
+            else if (faceTotal == F8.felt){
+                player2.balance += F8.point;
+                System.out.println("Du er landet på " + F8.name + ", og du har fået " + F8.point + " point.");
+            }
+            else if (faceTotal == F9.felt){
+                player2.balance += F9.point;
+                System.out.println("Du er landet på " + F9.name + ", og du har fået " + F9.point + " point.");
+            }
+            else if (faceTotal == F10.felt){
+                player2.balance += F10.point;
+                System.out.println("Du er landet på " + F10.name + ", og du har fået " + F10.point + " point.");
+            }
+            else if (faceTotal == F11.felt){
+                player2.balance += F11.point;
+                System.out.println("Du er landet på " + F11.name + ", og du har fået " + F11.point + " point.");
+            }
+            else if (faceTotal == F12.felt){
+                player2.balance += F12.point;
+                System.out.println("Du er landet på " + F12.name + ", og du har fået " + F12.point + " point.");
+            }
+            else {
+                System.out.println("Der findes kun felter 2-12. Du har slået " + faceTotal);
+            }
+            System.out.println(player2.name + "'s nye score er: " + player2.balance);
+        }
+
+        if (player1.balance > 3000 && player2.balance > 3000){
+            System.out.println("Tillykke " + player1.name + " og " + player2.name + ", i har begge vundet! Den sidste score er:\n" + player1.name + ":" + player1.balance + "\n" + player2.name + ":" + player2.balance);
+        }
+        else if (player2.balance > 3000){
+            System.out.println("Tillykke " + player2.name + "! Du vandt, og den " +
+                    "sidste score blev:\n" +player1.name + ":" + player1.balance +
+                    "\n" + player2.name + ":" + player2.balance);
+        }
+        else if (player1.balance > 3000){
+            System.out.println("Tillykke" + player1.name + "! Du vandt, og den " +
+                    "sidste score blev:\n" +player1.name + ":" + player1.balance +
+                    "\n" + player2.name + ":" + player2.balance);
         }
     }
 }
