ce73d15ffb2fdfa112124bf99fa75c5d8b7dd4dd
Kasper Hansen <kasper1999.hansen@gmail.com>
2021-10-27 15:58:04 +0200

    Rettelser i Konto og Player klassen.

--- src/Konto.java
+++ src/Konto.java
@@ -1,15 +1,8 @@
 public class Konto {
     private int balance;
 
-    public Konto(int balance){
+    public Konto(){
         this.balance = 1000;
-    }
-
-    public int addScore(int point){ // Laver et ny variabel med typen Felt
-        this.balance += point;
-        if (balance < 0)
-            balance = 0;
-        return this.balance;
     }
 
     public int getBalance() {
--- src/Player.java
+++ src/Player.java
@@ -1,15 +1,25 @@
 public class Player {
     private String name;
+    private Konto konto;
+    private int balance;
 
-    public Player(String name){
+    public Player(String name) {
+        this.name = name;
+        this.konto = new Konto();
+    }
+
+    public int addScore(int point){ // Laver et ny variabel med typen Felt
+        this.balance += point;
+        if (balance < 0)
+            balance = 0;
+        return this.balance;
+    }
+
+    public void setName(String name) {
         this.name = name;
     }
 
-    public void setName(String name){
-        this.name=name;
-    }
-
-    public String getName(){
+    public String getName() {
         return name;
     }
 
