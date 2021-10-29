c920a3ec5c5db015e383f13c9f5ca9ed1d2cfbef
Silja Sandersen <silja.sandersen@hotmail.com>
2021-10-29 10:15:14 +0200

    KontoTest: Navneændring i playerens navn fra a til en tom String

--- src/KontoTest.java
+++ src/KontoTest.java
@@ -6,7 +6,7 @@ public class KontoTest {
 
     @Test
     public void playerBalanceNeverBelowZero(){
-        Player player = new Player("a");
+        Player player = new Player("");
         player.setBalance(0);
         player.addScore(-100);
         boolean kontoTjek = player.getBalance() < 0;
@@ -15,14 +15,14 @@ public class KontoTest {
 
     @Test
     public void balanceAddsSetAmount(){
-        Player player = new Player("a");
+        Player player = new Player("");
         player.addScore(100);
         assertEquals(1100, player.getBalance());
     }
 
     @Test
     public void balanceRemovesSetAmount(){
-        Player player = new Player("a");
+        Player player = new Player("");
         player.addScore(-500);
         assertEquals(500, player.getBalance());
     }
